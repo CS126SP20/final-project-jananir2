ee7b71ff3c39ad9ec55f5b830bb1c437f75b913f
Janani Rajadurai <janani.rajadurai@gmail.com>
2020-04-26 23:38:23 -0400

    Added nanogui library and created a setup class

---
+++ include/mylibrary/setup.h
@@ -0,0 +1,16 @@
+//
+// Created by janan on 4/26/2020.
+//
+
+#ifndef FINALPROJECT_SETUP_H
+#define FINALPROJECT_SETUP_H
+
+#include <nanogui/button.h>
+
+using namespace nanogui;
+
+class setup {
+//  Button button;
+};
+
+#endif  // FINALPROJECT_SETUP_H
---
+++ src/setup.cpp
@@ -0,0 +1,5 @@
+//
+// Created by janan on 4/26/2020.
+//
+
+#include "mylibrary/setup.h"
