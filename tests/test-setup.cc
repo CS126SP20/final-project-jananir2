2107493c7b8fa1b1580a5478337c16b59399bd7e
Janani Rajadurai <janani.rajadurai@gmail.com>
2020-05-04 18:56:56 -0400

    Completed testing for set up class using simple file

---
+++ tests/test-setup.cc
@@ -0,0 +1,42 @@
+// Copyright (c) 2020 CS126SP20. All rights reserved.
+
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+#include <cinder/Rand.h>
+#include <mylibrary/setup.h>
+
+#include <mylibrary/engine.h>
+
+
+TEST_CASE("Check Setup methods", "[simplefile]") {
+  using namespace std;
+
+  mylibrary::Setup setup_{};
+  setup_.PopulateQAvectors("simplefile.csv");
+
+  SECTION("Check correct number of questions") {
+    REQUIRE(setup_.GetNumQuestions() == 2);
+  }
+
+  SECTION("Check correct answers") {
+    REQUIRE(setup_.RetrieveAnswers().at(0).find(
+                "C) green") != std::string::npos);
+    REQUIRE(setup_.RetrieveAnswers().at(1).find("A) moo") != std::string::npos);
+  }
+
+  SECTION("Check correct questions") {
+    REQUIRE(setup_.RetrieveQuestions().at(0).at(0).find(
+                "What color is grass?") != std::string::npos);
+    REQUIRE(setup_.RetrieveQuestions().at(1).at(0).find(
+                "What sound does a cow make?") != std::string::npos);
+  }
+
+  SECTION("Check correct choices") {
+    REQUIRE(setup_.RetrieveQuestions().at(0).at(3).find(
+        "C) green") != std::string::npos);
+    REQUIRE(setup_.RetrieveQuestions().at(1).at(4).find(
+        "D) boo") != std::string::npos);
+  }
+
+}
